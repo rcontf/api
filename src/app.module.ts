460fbd2a299756ecb6a86669c800ecfef73fae18
c43721 <rlahman.game@gmail.com>
2020-12-05 16:30:18 -0600

    Add joi, config, helmet, and cookie parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [TestModule],
+  imports: [
+    TestModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        STEAM_API_KEY: Joi.string().trim().required(),
+        JWT_SECRET: Joi.string().trim().required(),
+        PORT: Joi.number().failover(8080).default(8080),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
