8082a32b4c08292087df42eeedf033247213db6f
c43721 <rlahman.game@gmail.com>
2020-12-08 14:49:42 -0600

    Rename execute DTO file

--- src/execute/execute.controller.ts
+++ src/execute/execute.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ExecuteService } from './execute.service';
-import { ExecuteCommandDto } from './dto/create-execute.dto';
+import { ExecuteCommandDto } from './dto/execute.dto';
 
 @Controller('execute')
 export class ExecuteController {
--- src/execute/execute.service.ts
+++ src/execute/execute.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ExecuteCommandDto } from './dto/create-execute.dto';
+import { ExecuteCommandDto } from './dto/execute.dto';
 import Rcon from './rcon/rcon';
 
 @Injectable()
