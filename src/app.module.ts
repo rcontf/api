bc231deb9e62f87c3d48062afb62e9f96111d669
c43721 <rlahman.game@gmail.com>
2020-12-12 17:41:46 -0600

    Remove test module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
-import { TestModule } from './test/test.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
