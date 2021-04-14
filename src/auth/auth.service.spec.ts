e90d3462acd90ec79505202794dffbeb505fdbb3
c43721 <rlahman.game@gmail.com>
2021-04-14 17:50:09 -0500

    fix(test): fix all tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 
+import { verify } from 'jsonwebtoken';
+
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: 'JWT_SECRET',
+          useValue: 'secret',
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +22,13 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('#generateToken', () => {
+    it('can sign a token', () => {
+      const token = service.generateToken('the_unique_id');
+
+      expect(verify(token, 'secret')).toBeTruthy();
+    });
   });
 });
